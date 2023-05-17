e84c1fc96975691ffe97e17e4ce6e6fae14e0d24
akhilquotes <120096234+akhilquotes@users.noreply.github.com>
2023-05-17 15:53:59 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     // domains: ["gateway.pinata.cloud"],
+    staticPageGenerationTimeout: 1000,
     remotePatterns: [
       {
         protocol: "https",
